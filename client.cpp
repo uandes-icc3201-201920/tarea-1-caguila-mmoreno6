21c28a6761b0936490014f0163aa348d9f0ea188
martinmorenoc <43624675+martinmorenoc@users.noreply.github.com>
2019-09-04 13:40:31 -0400

    Problema con conexion

--- client.cpp
+++ client.cpp
@@ -1,21 +1,85 @@
 #include <iostream>
+#include <stdio.h>
 #include <memory>
-#include <string>
+#include <string.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include "util.h"
+#include <stdlib.h>
 
 using namespace std;
+char *socket_path = (char *)"\0hidden";
 
 int main(int argc, char** argv) {
-	
-	string cmd = "";
+  struct sockaddr_un addr;
+  char buf[100];
+  int fd,rc;
+
+	int sflag = 0;
+	int opt;
+
+	// Procesar opciones de linea de comando
+    while ((opt = getopt (argc, argv, "s:")) != -1) {
+        switch (opt)
+		{
+			/* Procesar el flag s si el usuario lo ingresa */
+			case 's':
+				socket_path=argv[2];
+				sflag = 1;
+				break;
+			default:
+				cout << "input invalido" << endl;
+				return 0;
+          }	    	
+    }
+		string cmd = "";
 	
 	while (cmd != "quit") {
 		cout << ">";
 		cin >> cmd;
-	}
+		if (cmd == "connect") {
+			if ( (fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
+				cout << "socket error" << endl;
+				continue;
+			}
+		
+			memset(&addr, 0, sizeof(addr));
+			addr.sun_family = AF_UNIX;
+			if (*socket_path == '\0') {
+			*addr.sun_path = '\0';
+				socket_path = (char *)"/tmp/db.tuples.sock";
+				strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path)-1);
+			} 
+			else {
+				strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path)-1);
+			}
 
+			if (connect(fd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
+				cout <<"connect error" << endl;
+				exit(-1);
+			}
+			else { cout << "connection success" << endl; }
+			continue;
+		}
+		
+		if (cmd == "disconnect") {
+			if (fd > 0) {
+				close(fd);
+				cout << "disconnection success" << endl;
+				}
+			continue;
+			}
+		
+		if (strncmp(cmd.c_str(), "insert", 6)==0 || strncmp(cmd.c_str(), "get", 3)==0) {
+			
+				write(fd, cmd.c_str(), 10000);
+				
+				continue;
+			}
+
+	
+	continue;
+	}
 	return 0;	
 }
--- server.cpp
+++ server.cpp
@@ -1,32 +1,120 @@
 #include <iostream>
+#include <sstream>
+#include <stdio.h>
 #include <memory>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include "util.h"
-
+#include <stdlib.h>
+#include <string.h>
 using namespace std;
 
 // Almacenamiento KV
 KVStore db;
+char* socket_path = (char *)"\0hidden";
 
 int main(int argc, char** argv) {	
-	
+	struct sockaddr_un addr;
+	char buf[100];
+	int fd,cl,rc;
 	int sflag = 0;
 	int opt;
-	
+
 	// Procesar opciones de linea de comando
     while ((opt = getopt (argc, argv, "s:")) != -1) {
         switch (opt)
 		{
 			/* Procesar el flag s si el usuario lo ingresa */
 			case 's':
+				socket_path=argv[2];
 				sflag = 1;
 				break;
 			default:
-				return EXIT_FAILURE;
+				cout << "input invalido" << endl;
+				return 0;
           }	    	
     }
+
+  if ((fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
+    cout <<"socket error" <<endl;
+    exit(-1);
+  }
+
+  memset(&addr, 0, sizeof(addr));
+  addr.sun_family = AF_UNIX;
+  if (*socket_path == '\0') {
+	*addr.sun_path = '\0';
+    socket_path = (char *)"/tmp/db.tuples.sock";
+    strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path)-1);
+  } else {
+    strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path)-1);
+    unlink(socket_path);
+  }
+
+  if (bind(fd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
+    cout <<"bind error: socket ya existe en esta direccion" << endl;
+    exit(-1);
+  }
+  if (listen(fd, 5) == -1) {
+    cout <<"listen error" << endl;
+    exit(-1);
+  }
+	else {
+		cout << "Escuchando conexion del cliente..." << endl;
+	}
+
+  while (1) {
+    if ( (cl = accept(fd, NULL, NULL)) == -1) {
+      cout << "accept error" <<endl;
+      continue;
+    }
+		else {
+			cout << "Conexion con cliente aceptada\nEsperando comandos del clientes..." <<endl;
+		}
+
+    while ( (rc=read(cl,buf,sizeof(buf))) > 0) { 
+      printf("%.*s\n", rc, buf);
+      string copy(buf);
+      //Identifica los valores a y b de insert(a,b)
+			if (strncmp(buf, "insert", 6)==0) {
+				string parentesis = copy.substr(6);	
+				int posComa = parentesis.find(',');
+				if (posComa != string::npos)
+				  {		
+					int posUltimoParentesis = parentesis.find(')');
+					string primerNumero = parentesis.substr(1,posComa-1);
+					string segundoNumero = parentesis.substr(posComa+1, (posUltimoParentesis-posComa-1));
+					cout << primerNumero << endl;
+					cout << segundoNumero << endl;
+					*buf = '\0';
+				}
+				else 
+				{
+					int ultParentesis = parentesis.find(')');
+					string primerNumero = parentesis.substr(1,ultParentesis-1);
+					cout << primerNumero << endl;
+					*buf = '\0';
+			}
+			}
+			//Identifica el valor a de get(a)
+			else if (strncmp(buf, "get", 3)==0) {
+				string parentesis = copy.substr(3);
+				int ultParentesis = parentesis.find(')');
+				string primerNumero = parentesis.substr(1,ultParentesis-1);
+				cout << primerNumero << endl;
+				*buf = '\0';
+				}
+			}
+    if (rc == -1) {
+      cout << "read error" << endl;
+      exit(-1);
+    }
+    else if (rc == 0) {
+      cout << "EOF\n" << endl;
+      close(cl);
+    }
+  }
 	
 	// Uso elemental del almacenamiento KV:
 	
