4e82a1cc88b367a5f7fdbca23b0d786417f60181
martinmorenoc <43624675+martinmorenoc@users.noreply.github.com>
2019-09-23 23:50:12 -0300

    Compatible con multiples clientes

--- server.cpp
+++ server.cpp
@@ -8,24 +8,249 @@
 #include "util.h"
 #include <stdlib.h>
 #include <string.h>
+#include <string> 
+#include <pthread.h>
+#include <time.h>
 using namespace std;
 
 // Almacenamiento KV
 KVStore db;
 
+char client_message[2000];
+
+int fd,cl,rc;
+int sflag = 0;
+int opt;
+int ex = 0;
+pthread_t tid[60];
+int i = 0;
+string primerNumero;
+string segundoNumero;
+struct arg_struct {
+    pthread_rwlock_t *arg1;
+    int arg2;
+};
+
+pthread_rwlock_t lock_rw = PTHREAD_RWLOCK_INITIALIZER;
+
+
+
+void * socketThread(void *args)
+{
+  char buf[1024];	
+  struct arg_struct *arg = (struct arg_struct *)args;	
+  int newSocket = arg -> arg2;
+  pthread_t thId = pthread_self();
+  std::cout << "Thread Id from thread function : " << thId << std::endl;
+  pthread_rwlock_t *p = (pthread_rwlock_t *)arg -> arg1;	
+  while ( (rc=read(newSocket,buf,sizeof(buf))) > 0) { 
+ 			if (strncmp(buf, "quit", 4)==0 && strlen(buf) == 4) {
+			cout << "quiting..." << endl;
+			ex = 1;
+			break;
+				}
+      printf("%.*s\n", rc, buf);
+      string copy(buf);
+      //Identifica los valores a y b de insert(a,b)
+      pthread_t thId = pthread_self();
+      std::cout << "Thread Id from thread function : " << thId << std::endl;
+	if (strncmp(buf, "insert", 6)==0) {
+		string parentesis = copy.substr(6);
+		int posComa = parentesis.find(',');
+                cout << posComa;
+				if (posComa != string::npos)
+				  {		
+					int posUltimoParentesis = parentesis.find(')');
+					string primerNumero = parentesis.substr(1,posComa-1);
+					string segundoNumero = parentesis.substr(posComa+1, (posUltimoParentesis-posComa-1));
+					cout << "key: "+primerNumero << endl;
+					cout << "value: "+segundoNumero << endl;
+					char cstr[primerNumero.size() + 1];
+					strcpy(cstr, primerNumero.c_str());
+					Value valuee = { segundoNumero.size(), segundoNumero };
+					unsigned long key = strtoul(cstr, NULL, 0);
+					pthread_rwlock_wrlock(p);
+					db.insert(std::pair<unsigned long, Value>(key, valuee));
+					*buf = '\0';
+					pthread_rwlock_unlock(p);
+					pthread_rwlock_rdlock(p);
+					string msg = "Se guardo el valor " + db[key].data + " en la key " + primerNumero;
+					send(newSocket, msg.c_str(), strlen(msg.c_str()),0);
+					pthread_rwlock_unlock(p);
+					
+				}
+				else 
+				{
+					int ultParentesis = parentesis.find(')');
+					string primerNumero = parentesis.substr(1,ultParentesis-1);
+					//CREAR KEY ALEATORIA
+					cout << "value: "+primerNumero << endl;
+					*buf = '\0';
+			}
+			}
+			//Identifica el valor a de get(a)
+			else if (strncmp(buf, "get", 3)==0) {
+				string parentesis = copy.substr(3);
+				int ultParentesis = parentesis.find(')');
+				string primerNumero = parentesis.substr(1,ultParentesis-1);
+				char cstr2[primerNumero.size() + 1];
+				strcpy(cstr2, primerNumero.c_str());
+				unsigned long key = strtoul(cstr2, NULL, 0);
+				map<unsigned long,Value>::iterator it;
+				it = db.find(key);
+				if (it == db.end()) {
+				 	string msg = "No existe un value asociado a esa key";
+				 	send(newSocket, msg.c_str(), strlen(msg.c_str()),0);
+				 	}
+				else {
+					string msg= "El valor asociado al key " + primerNumero + " es: " + db[key].data;
+					send(newSocket, msg.c_str(), strlen(msg.c_str()),0);
+				}
+				*buf = '\0';
+				}
+			//Identifica el valor a de peek(a)
+			else if (strncmp(buf, "peek", 4)==0) {
+				string parentesis = copy.substr(4);
+				int ultParentesis = parentesis.find(')');
+				string primerNumero = parentesis.substr(1,ultParentesis-1);
+				map<unsigned long,Value>::iterator it;
+				char cstr2[primerNumero.size() + 1];
+				strcpy(cstr2, primerNumero.c_str());
+				unsigned long key = strtoul(cstr2, NULL, 0);
+				it = db.find(key);
+				if (it == db.end()) {
+				 	string msg = "false";
+				 	send(newSocket, msg.c_str(), strlen(msg.c_str()),0);
+				 	}
+				else {
+					string msg = "true";
+					send(newSocket, msg.c_str(), strlen(msg.c_str()),0);
+				}
+				*buf = '\0';
+				}
+			//Identifica el valor a y b de update(a,b)
+			else if (strncmp(buf, "update", 6)==0) {
+				string parentesis = copy.substr(6);	
+				int posComa = parentesis.find(',');
+				int posUltimoParentesis = parentesis.find(')');
+				string primerNumero = parentesis.substr(1,posComa-1);
+				string segundoNumero = parentesis.substr(posComa+1, (posUltimoParentesis-posComa-1));
+				char cstr2[primerNumero.size() + 1];
+				map<unsigned long,Value>::iterator it;
+				strcpy(cstr2, primerNumero.c_str());
+				unsigned long key = strtoul(cstr2, NULL, 0);
+				pthread_rwlock_wrlock(p);
+				it = db.find(key);
+				if (it == db.end()) {
+				 	string msg = "Key no existe en la base de datos";
+				 	send(newSocket, msg.c_str(), strlen(msg.c_str()),0);
+				 	}
+				else {
+					Value valuee = { segundoNumero.size(), segundoNumero };
+					db[key] = valuee;
+					string msg = "Valor actualizado";
+					send(newSocket, msg.c_str(), strlen(msg.c_str()),0);
+				}
+				*buf = '\0';
+				pthread_rwlock_unlock(p);
+
+				}
+			//Identifica el valor a de delete(a)
+			else if (strncmp(buf, "delete", 6)==0) {
+				string parentesis = copy.substr(6);
+				int ultParentesis = parentesis.find(')');
+				string primerNumero = parentesis.substr(1,ultParentesis-1);
+				map<unsigned long,Value>::iterator it;
+				char cstr2[primerNumero.size() + 1];
+				strcpy(cstr2, primerNumero.c_str());
+				unsigned long key = strtoul(cstr2, NULL, 0);
+				pthread_rwlock_wrlock(p);
+				it = db.find(key);
+				if (it == db.end()) {
+				 	string msg = "No existe";
+				 	send(newSocket, msg.c_str(), strlen(msg.c_str()),0);
+				 	}
+				else {
+					db.erase(key);
+					string msg = "Eliminado";
+				}
+				*buf = '\0';
+				pthread_rwlock_unlock(p);
+				}
+			//Retorna list de keys
+			else if (strncmp(buf, "list", 4)==0) {
+				pthread_rwlock_rdlock(p);
+				string msg = "Keys disponibles en la BD:\n";
+				for(map<unsigned long, Value>::iterator it = db.begin(); it != db.end(); ++it) {
+					char buffer [sizeof(unsigned long)*8+1];
+					//ultoa(it->first,buffer,10);
+					//msg += s(buffer) + "\n";
+				}
+				//send(newSocket, msg.c_str(), strlen(msg.c_str()),0);
+				*buf = '\0';
+				}
+				pthread_rwlock_unlock(p);
+			}
+    if (rc == -1) {
+      cout << "read error" << endl;
+      close(newSocket);
+      pthread_exit(NULL);
+    }
+    else if (rc == 0) {
+      cout << "EOF\n" << endl;
+      close(newSocket);
+    }
+    if (ex == 1){
+		cout << "caga por el ex" << endl;
+		pthread_exit(NULL);
+	} 
+	cout << "no caga por el ex" << endl;
+
+    // Uso elemental del almacenamiento KV:
+	
+	// Creamos un arreglo de bytes a mano
+	//byte data[] = { 0x01, 0x01, 0x01, 0x01, 0x01 };
+
+	// Luego un vector utilizando el arreglo de bytes
+	//vector<byte> vdata(data, data + sizeof(data));
+	
+	// Creamos el valor
+
+	//Value val = { segundoNumero.size(), segundoNumero };
+	
+	// Insertamos un par clave, valor directamente
+	// en el mapa KV
+
+    //pthread_mutex_lock(&lock);
+
+	//unsigned long ul;
+	//ul = strtoul (primerNumero.c_str(), NULL, 0);
+	
+	// Nota: Debiera diseñarse una solución más robusta con una interfaz
+	// adecuada para acceder a la estructura.
+	//db.insert(std::pair<unsigned long, Value>(1000, val));
+	//db.insert(std::pair<unsigned long, Value>(ul, val));	
+	// Imprimir lo que hemos agregado al mapa KV.
+	
+	//cout << "inserto y eso es lo que devuelve" << endl;
+	//cout << db[ul].size << " " << db[ul].data << endl;
+    //pthread_mutex_unlock(&lock);
+	return 0;
+}
+
 char* socket_path = (char *)"\0hidden";
+
 
 int main(int argc, char** argv) {	
 	struct sockaddr_un addr;
-	char buf[100];
-	int fd,cl,rc;
-	int sflag = 0;
-	int opt;
-	int ex = 0;
-
+    	struct sockaddr_storage serverStorage;
+    	socklen_t addr_size;	
+    	cout << socket_path;
+	srand (time(NULL));
+	int randomKey = rand()% 9001 + 1000;
 	// Procesar opciones de linea de comando
-    while ((opt = getopt (argc, argv, "s:")) != -1) {
-        switch (opt)
+    	while ((opt = getopt (argc, argv, "s:")) != -1) {
+        	switch (opt)
 		{
 			/* Procesar el flag s si el usuario lo ingresa */
 			case 's':
@@ -38,145 +263,70 @@ int main(int argc, char** argv) {
           }	    	
     }
 	while (1) {
-  if ((fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
-    cout <<"socket error" <<endl;
-    exit(-1);
-  }
+        if ((fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
+            cout <<"socket error" <<endl;
+            exit(-1);
+        }
 
-  memset(&addr, 0, sizeof(addr));
-  addr.sun_family = AF_UNIX;
-  if (*socket_path == '\0') {
-	*addr.sun_path = '\0';
-    socket_path = (char *)"/tmp/db.tuples.sock";
-    strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path)-1);
-  } else {
-    strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path)-1);
-    unlink(socket_path);
-  }
+        memset(&addr, 0, sizeof(addr));
+        addr.sun_family = AF_UNIX;
+        if (*socket_path == '\0') {
+            *addr.sun_path = '\0';
+            socket_path = (char *)"/tmp/db.tuples.sock";
+            strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path)-1);
+        } else {
+            strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path)-1);
+            unlink(socket_path);
+        }
 
-  if (bind(fd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
-    cout <<"bind error: socket ya existe en esta direccion" << endl;
-    exit(-1);
-  }
-  if (listen(fd, 5) == -1) {
-    cout <<"listen error" << endl;
-    exit(-1);
-  }
-	else {
-		cout << "Escuchando conexion del cliente..." << endl;
-	}
+        if (bind(fd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
+            cout <<"bind error: socket ya existe en esta direccion" << endl;
+            exit(-1);
+        }
+        cout << "si llega";
+        if (listen(fd, 5) == -1) {
+            cout <<"listen error" << endl;
+            i = 0;
+        }
+            else {
+                cout << "Escuchando conexion del cliente..." << endl;
+            }
 
   while (1) {
-    if ( (cl = accept(fd, NULL, NULL)) == -1) {
+
+
+        addr_size = sizeof serverStorage;
+        int newSocket = accept(fd, (struct sockaddr *) &serverStorage, &addr_size);
+		struct arg_struct args;
+    	args.arg1 = &lock_rw;
+    	args.arg2 = newSocket;
+        if( pthread_create(&tid[i], NULL, socketThread, (void *)&args) != 0 )
+            printf("Failed to create thread\n");
+        else {
+			cout << "Conexion con cliente aceptada\nEsperando comandos del clientes..." <<endl;
+		}
+
+            if( i >= 5)
+            {
+            i = 0;
+            while(i < 5)
+            {
+                pthread_join(tid[i++],NULL);
+            }
+            i = 0;
+            }
+
+
+    /* if ( (cl = accept(fd, NULL, NULL)) == -1) {
       cout << "accept error" <<endl;
       continue;
     }
 		else {
 			cout << "Conexion con cliente aceptada\nEsperando comandos del clientes..." <<endl;
 		}
+        */
 
-    while ( (rc=read(cl,buf,sizeof(buf))) > 0) { 
- 			if (strncmp(buf, "quit", 4)==0 && strlen(buf) == 4) {
-			cout << "quiting..." << endl;
-			ex = 1;
-			break;
-				}
-      printf("%.*s\n", rc, buf);
-      string copy(buf);
-      //Identifica los valores a y b de insert(a,b)
-			if (strncmp(buf, "insert", 6)==0) {
-				string parentesis = copy.substr(6);	
-				int posComa = parentesis.find(',');
-				if (posComa != string::npos)
-				  {		
-					int posUltimoParentesis = parentesis.find(')');
-					string primerNumero = parentesis.substr(1,posComa-1);
-					string segundoNumero = parentesis.substr(posComa+1, (posUltimoParentesis-posComa-1));
-					cout << "key: "+primerNumero << endl;
-					cout << "value: "+segundoNumero << endl;
-					*buf = '\0';
-				}
-				else 
-				{
-					int ultParentesis = parentesis.find(')');
-					string primerNumero = parentesis.substr(1,ultParentesis-1);
-					cout << "value: "+primerNumero << endl;
-					*buf = '\0';
-			}
-			}
-			//Identifica el valor a de get(a)
-			else if (strncmp(buf, "get", 3)==0) {
-				string parentesis = copy.substr(3);
-				int ultParentesis = parentesis.find(')');
-				string primerNumero = parentesis.substr(1,ultParentesis-1);
-				cout << "key: " +primerNumero << endl;
-				*buf = '\0';
-				}
-			//Identifica el valor a de peek(a)
-			else if (strncmp(buf, "peek", 4)==0) {
-				string parentesis = copy.substr(4);
-				int ultParentesis = parentesis.find(')');
-				string primerNumero = parentesis.substr(1,ultParentesis-1);
-				cout << "key: "+primerNumero << endl;
-				*buf = '\0';
-				}
-			//Identifica el valor a y b de update(a,b)
-			else if (strncmp(buf, "update", 6)==0) {
-				string parentesis = copy.substr(6);	
-				int posComa = parentesis.find(',');
-				int posUltimoParentesis = parentesis.find(')');
-				string primerNumero = parentesis.substr(1,posComa-1);
-				string segundoNumero = parentesis.substr(posComa+1, (posUltimoParentesis-posComa-1));
-				cout << "key: "+primerNumero << endl;
-				cout << "value: "+segundoNumero << endl;
-				*buf = '\0';
-				}
-			//Identifica el valor a de delete(a)
-			else if (strncmp(buf, "delete", 6)==0) {
-				string parentesis = copy.substr(6);
-				int ultParentesis = parentesis.find(')');
-				string primerNumero = parentesis.substr(1,ultParentesis-1);
-				cout << "key: "+primerNumero << endl;
-				*buf = '\0';
-				}
-			//Retorna list de keys
-			else if (strncmp(buf, "list", 4)==0) {
-				cout << "lista: " << endl;
-				*buf = '\0';
-				}
-			}
-    if (rc == -1) {
-      cout << "read error" << endl;
-      exit(-1);
     }
-    else if (rc == 0) {
-      cout << "EOF\n" << endl;
-      close(cl);
-    }
-    if (ex == 1) break;
-  }
   }
 	
-	// Uso elemental del almacenamiento KV:
-	
-	// Creamos un arreglo de bytes a mano
-	byte data[] = { 0x01, 0x01, 0x01, 0x01, 0x01 };
-
-	// Luego un vector utilizando el arreglo de bytes
-	vector<byte> vdata(data, data + sizeof(data));
-	
-	// Creamos el valor
-	Value val = { 5, vdata };
-	
-	// Insertamos un par clave, valor directamente
-	// en el mapa KV
-	
-	// Nota: Debiera diseñarse una solución más robusta con una interfaz
-	// adecuada para acceder a la estructura.
-	db.insert(std::pair<unsigned long, Value>(1000, val));
-		
-	// Imprimir lo que hemos agregado al mapa KV.
-	cout << db[1000].size << " " << (int) db[1000].data[0] << endl;
-	
-	return 0;
 }
